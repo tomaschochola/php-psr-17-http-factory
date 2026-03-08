bab7f04b2fba6b9a6674112e2a9d09e494f21fc2
Tomáš Chochola <tomaschochola@tomaschochola.cz>
2026-03-08 04:32:42 +0100

    Changes of the day 2026-03-08T04:32:42+01:00

---
+++ src/HttpFactoryManifest.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * @author Tomáš Chochola <tomaschochola@tomaschochola.cz>
+ * @copyright © 2026 Tomáš Chochola <tomaschochola@tomaschochola.cz>
+ *
+ * @license CC-BY-ND-4.0
+ *
+ * @see {@link https://creativecommons.org/licenses/by-nd/4.0/} License
+ * @see {@link https://github.com/tomaschochola} GitHub Profile
+ * @see {@link https://github.com/sponsors/tomaschochola} GitHub Sponsors
+ */
+
+declare(strict_types=1);
+
+namespace TomasChochola\Psr\Http\Factory;
+
+use IteratorAggregate;
+use NoDiscard;
+use Override;
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ServerRequestFactoryInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+use Psr\Http\Message\UriFactoryInterface;
+use Traversable;
+
+/**
+ * @no-named-arguments
+ *
+ * @implements IteratorAggregate<mixed, mixed>
+ */
+readonly class HttpFactoryManifest implements IteratorAggregate
+{
+    #[NoDiscard]
+    #[Override]
+    public function getIterator(): Traversable
+    {
+        yield RequestForge::class => [RequestForge::class, 'unload'];
+        yield RequestFactoryInterface::class => [RequestForge::class, 'unload'];
+
+        yield ResponseForge::class => [ResponseForge::class, 'unload'];
+        yield ResponseFactoryInterface::class => [ResponseForge::class, 'unload'];
+
+        yield ServerRequestForge::class => [ServerRequestForge::class, 'unload'];
+        yield ServerRequestFactoryInterface::class => [ServerRequestForge::class, 'unload'];
+
+        yield ServerRequestInterface::class => [ServerRequestForge::class, 'produce'];
+
+        yield StreamForge::class => [StreamForge::class, 'unload'];
+        yield StreamFactoryInterface::class => [StreamForge::class, 'unload'];
+
+        yield UriForge::class => [UriForge::class, 'unload'];
+        yield UriFactoryInterface::class => [UriForge::class, 'unload'];
+    }
+}
